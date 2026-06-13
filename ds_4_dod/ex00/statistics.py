6a19bc25efb64f3063a7fe9bf06b3256ddc8a772
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2026-06-14 00:36:51 +0200

    firts flake correction

--- ds_4_dod/ex00/statistics.py
+++ ds_4_dod/ex00/statistics.py
@@ -1,11 +1,13 @@
 from typing import Any
 
+
 def ft_mean(*args: Any) -> float:
     n = len(args)
     sum = 0
     for value in args:
         sum += value
     return sum / n
+
 
 def ft_median(args: Any) -> float:
     sorted_values = sorted(args)
@@ -15,10 +17,9 @@ def ft_median(args: Any) -> float:
     else:
         return (sorted_values[n // 2 - 1] + sorted_values[n // 2]) / 2
 
+
 def ft_statistics(*args: Any, **kwargs: Any) -> None:
-    assert all(isinstance(valor, int) or isinstance(valor, float) for valor in args)
+    assert all(isinstance(valor, int) or
+               isinstance(valor, float) for valor in args)
     print(f"mean: {ft_mean(args)}")
     print(f"median: {ft_median(args)}")
-
-
-                 
